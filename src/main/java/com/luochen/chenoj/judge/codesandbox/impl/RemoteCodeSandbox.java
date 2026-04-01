386b5730b5d4bed49ad46ac4768f9bf54bb2f1df
luochen <luochen@victorysoft.com.cn>
2026-04-02 00:11:27 +0800

    远程代码沙箱的开发

--- src/main/java/com/luochen/chenoj/judge/codesandbox/impl/RemoteCodeSandbox.java
+++ src/main/java/com/luochen/chenoj/judge/codesandbox/impl/RemoteCodeSandbox.java
@@ -1,16 +1,35 @@
 package com.luochen.chenoj.judge.codesandbox.impl;
 
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONUtil;
+import com.luochen.chenoj.common.ErrorCode;
+import com.luochen.chenoj.exception.BusinessException;
 import com.luochen.chenoj.judge.codesandbox.CodeSandbox;
 import com.luochen.chenoj.judge.codesandbox.model.ExecuteCodeRequest;
 import com.luochen.chenoj.judge.codesandbox.model.ExecuteCodeResponse;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * 远程代码沙箱
  */
 public class RemoteCodeSandbox implements CodeSandbox {
+    //定义鉴权请求头和密钥
+    private static final String AUTH_REQUEST_HEADER="auth";
+
+    private static final String AUTH_ReQUEST_SECRET="secretKey";
     @Override
     public ExecuteCodeResponse executeCode(ExecuteCodeRequest request) {
         System.out.println("远程代码沙箱");
+        String url="http://localhost:8090/executeCode";
+        String json= JSONUtil.toJsonStr(request);
+        String responseStr= HttpUtil.createPost(url)
+                .header(AUTH_REQUEST_HEADER,AUTH_ReQUEST_SECRET)
+                .body(json)
+                .execute()
+                .body();
+        if(StringUtils.isBlank(responseStr)){
+            throw new BusinessException(ErrorCode.API_REQUEST_ERROR, "executeCode error"+"responseStr is null");
+        }
         return null;
     }
 }
